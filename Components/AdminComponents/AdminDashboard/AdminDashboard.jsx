17b4b94f4de774e322d44ac5399e29afa20eb10d
Huzaifa Iqbal <huzaifaiqbal89@gmail.com>
2025-04-21 20:39:47 +0500

    changes updated

--- Components/AdminComponents/AdminDashboard/AdminDashboard.jsx
+++ Components/AdminComponents/AdminDashboard/AdminDashboard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./AdminDashboard.scss";
-// import UserChart from "@/components/ui/UserChart";
-// import CarChart from "@/components/ui/CarChart";
+// import UserChart from "@/Components/ui/UserChart";
+// import CarChart from "@/Components/ui/CarChart";
 import UserChart from "@/Components/charts/UserChart";
 import CarChart from "@/Components/charts/CarListingChart";
 import AdminCard from "@/DummyData/AdminCard.json";
